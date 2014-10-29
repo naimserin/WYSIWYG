4f18fec01e9dfe47ea70466c2154fbdbfb880380
Naim <naimserin@gmail.com>
2014-10-29 22:03:25 +0200

    Başlangıç WYSIWYG

---
+++ WYSIWYG/WYSIWYG/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WYSIWYG.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SayfaFileUpload(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine("" + Server.MapPath("/") + "Upload\\Sayfa\\", fileName);
+                    file.SaveAs(path);
+                }
+                
+                return Json(file.FileName + '|' + file.ContentType, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json("error", JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+    }
+}
